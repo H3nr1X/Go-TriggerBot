95b68ff99be7b98cc7e17eb1437e5e733423a998
H3nr1X <39547478+H3nr1X@users.noreply.github.com>
2020-09-08 13:28:11 +0300

    Cleaned up whitespace

--- Triggerbot.go
+++ Triggerbot.go
@@ -5,12 +5,8 @@ import (
 	"fmt"
 	"log"
 	"time"
-	"/ReadWriteMemory"
-
-
-	
+	"/ReadWriteMemory"	
 )
-
 const ( // https://github.com/frk1/hazedumper for offsets
 	dwLocalPlayer       = 0xd3fc5c
 	dwEntityList        = 0x4d5442c
@@ -20,14 +16,11 @@ const ( // https://github.com/frk1/hazedumper for offsets
 	dwForceAttack	    = 0x3185984
 )
 
-
-
 func main() {
 	process, err := ReadWriteMemory.ProcessByName("csgo")
 	if err != nil {
 		log.Panicf("csgo.exe not found. Error: %s", err.Error())
 	}
-
 	client := process.Modules["client.dll"].ModBaseAddr
 	if err != nil {
 		fmt.Println(err)
@@ -37,14 +30,11 @@ func main() {
 			time.Sleep(time.Millisecond * 100)
 			continue
 		}
+		
 		player, _ := process.ReadIntPtr(client + dwLocalPlayer)
-		
 		entity_id, _ := process.ReadIntPtr(player + m_iCrosshairId)
-		
 		entity, _ := process.ReadIntPtr(client + dwEntityList + (entity_id - 1) * 0x10)
-		
 		entity_team, _ := process.ReadIntPtr(entity + m_iTeamNum)
-		
 		player_team, _ := process.ReadIntPtr(player + m_iTeamNum)
 		
 		if gomem.IsKeyDown(0x10){ // 0x10 = Shift
