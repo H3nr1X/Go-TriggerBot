eb8ef60bd0a0fa41878fd67cf4d7ca6b650b4cdb
H3nr1X <39547478+H3nr1X@users.noreply.github.com>
2020-09-08 10:45:35 +0300

    Added comments and cleaned up the code.

--- Triggerbot.go
+++ Triggerbot.go
@@ -11,7 +11,7 @@ import (
 	
 )
 
-const (
+const ( // https://github.com/frk1/hazedumper for offsets
 	dwLocalPlayer       = 0xd3fc5c
 	dwEntityList        = 0x4d5442c
 	iGlowIndex          = 0xa438
@@ -38,12 +38,18 @@ func main() {
 			continue
 		}
 		player, _ := process.ReadIntPtr(client + dwLocalPlayer)
+		
 		entity_id, _ := process.ReadIntPtr(player + m_iCrosshairId)
+		
 		entity, _ := process.ReadIntPtr(client + dwEntityList + (entity_id - 1) * 0x10)
+		
 		entity_team, _ := process.ReadIntPtr(entity + m_iTeamNum)
+		
 		player_team, _ := process.ReadIntPtr(player + m_iTeamNum)
-		if gomem.IsKeyDown(0x10){
-			if entity_id > 0 && entity_id <= 64 && player_team != entity_team {
+		
+		if gomem.IsKeyDown(0x10){ // 0x10 = Shift
+			
+			if entity_id > 0 && entity_id <= 64 && player_team != entity_team { // Checks if the crosshair is on an entity and checks if that entity is an enemy player
 					process.WriteInt(client + dwForceAttack, 6)
 					time.Sleep(time.Millisecond * 120)
 		time.Sleep(time.Millisecond * 10)
